01e6e2bbd140ff750456d6c2fb261e338ac1455a
stevewquezada <stevewquezada@gmail.com>
2020-01-09 15:24:37 -0400

    added bootstrap grid system

--- src/templates/posts.js
+++ src/templates/posts.js
@@ -15,39 +15,33 @@ const nextUrl = (index + 1).toString()
 return (
     <Layout>
         <div className="blog">
-            <div className="listed__counter">
-                <h5><span>{pageCount}</span> Total pages - </h5> 
-                <h5><span>{pageCount * 3}</span> Total Results</h5>
+            <div className="listed__counter container">
+                <div className="row">
+                  <h5><span>{pageCount}</span> Total pages - </h5> 
+                  <h5><span>{pageCount * 3}</span> Total Results</h5>
+                </div>
             </div>
-            {group.map(({ node }) => (
-                <div key={node.id} className="blog__list" >
-                    {/* 
-                      Wordpress return the whole link and the redirections does not work
-                      properly, so, here we are using the function .replace to remove the 
-                      main url from the wordpress link. 
-
-                      And we are using process.env.GATSBY_WP_URL variable, that it's set on 
-                      the .env.development and .env.production
-                    */}
- 
-                    <div className={node.featured_media ? 'list__post__info' : 'list__post__info --full__width'  }>
-                      <p className="list__post__date"><small><b>{node.date}</b></small></p>
-                      <Link to={'/post/'+node.slug}  >
-                        <h1 className="list__post__title" dangerouslySetInnerHTML={{__html: node.title}}/>
-                      </Link>
-                      <div className="list__post__excerpt " dangerouslySetInnerHTML={{__html: node.content.substring(0, 280) }}/>
-                    </div>
-                    
+            {group.map(({ node }, index) => (
+                <div key={node.id}  className={index % 2 == 0 ? 'blog__list --right container' : 'blog__list --left container'}  >
+                    <div className="row">
+                      <div className={node.featured_media ? 'list__post__info col-xs-12 col-md-6' : 'list__post__info --full__width col-xs-12 col-md-12'  }>
+                        <p className="list__post__date"><small><b>{node.date}</b></small></p>
+                        <Link to={'/post/'+node.slug}  >
+                          <h1 className="list__post__title" dangerouslySetInnerHTML={{__html: node.title}}/>
+                        </Link>
+                        <div className="list__post__excerpt " dangerouslySetInnerHTML={{__html: node.content.substring(0, 280) }}/>
+                      </div>
                       {
                         node.featured_media
                         ?
-                        <div className="list__post__image">
+                        <div className="list__post__image col-xs-12 col-md-6">
                           <Link to={'/post/'+node.slug}  >
                             <img src={node.featured_media.localFile.childImageSharp.resolutions.src} alt={node.featured_media.alt_text} />
                           </Link>
                         </div>
                         : ''
                       }
+                    </div>
                 </div>
             ))}
             <PaginationController 
